565cc4e8420d038072c4a897ea299a5deebe73c8
Kenneth Rosas <kencrosas@gmail.com>
2022-01-29 08:14:19 -0500

    At 57:59 mark

--- app.js
+++ app.js
@@ -6,9 +6,26 @@ const {
 app.use(express.json());
 module.exports = app;
 
+const path = require("path");
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
 app.post("/api/auth", async (req, res, next) => {
   try {
-    res.send(await User.authenticate(req.body));
+    //This returns a token
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (ex) {
+    next(ex);
+  }
+});
+//Error handling. If status error, cool. Otherwise, it'll be a 500 error, and an object will be sent with an error message
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send({ error: err.message });
+});
+
+app.get("/api/auth", async (req, res, next) => {
+  try {
+    //sending a header or called authorization. This gives back a username
+    res.send(await User.byToken(req.headers.authorization));
   } catch (ex) {
     next(ex);
   }
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -4,7 +4,6 @@ const {
 } = require("../db");
 const jwt = require("jsonwebtoken");
 const { expect } = require("chai");
-const { application_name } = require("pg/lib/defaults");
 const app = require("supertest")(require("../app"));
 
 describe("Routes", () => {
@@ -24,6 +23,33 @@ describe("Routes", () => {
         expect(response.status).to.equal(200);
         //This is used with SuperTest
         expect(response.body.token).to.be.ok;
+      });
+    });
+  });
+  describe("POST /api/auth", () => {
+    describe("with INVALID credentials", () => {
+      it("returns a token", async () => {
+        const response = await app
+          .post("/api/auth")
+          .send({ username: "larry", password: "larry_pw1" });
+        expect(response.status).to.equal(401);
+        //USUALLY no res.body received because express naturally sends nothing when error-ing out
+        expect(response.body.error).to.equal("bad credentials");
+      });
+    });
+  });
+  describe("GET /api/auth", () => {
+    describe("with valid token", () => {
+      it("returns a user", async () => {
+        const token = await jwt.sign(
+          { id: seed.users.lucy.id },
+          process.env.JWT
+        );
+        const response = await app
+          .get("/api/auth")
+          .set({ authorization: token });
+        expect(response.status).to.equal(200);
+        expect(response.body.username).to.equal("lucy");
       });
     });
   });
